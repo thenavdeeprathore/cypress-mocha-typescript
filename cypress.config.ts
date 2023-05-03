ab54db35d3c43ccd83a5f58aa30a66bf59314044
thenavdeeprathore <thenavdeeprathore@gmail.com>
2023-05-03 21:59:31 +0530

    updated default cypress config settings

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 5000,
+    pageLoadTimeout: 60000,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
   },
 });
